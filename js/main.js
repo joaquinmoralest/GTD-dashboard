d0080dfb74ee76eab8caa9c59c69b041a8f0eae8
Joaquin Morales <joaquin.moralestr@gmail.com>
2022-02-03 23:46:33 -0300

    Definicion de varibles y clases a utilizar

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Variables
+const formulario = document.querySelector('form');
+const listadoInbox = document.querySelector('#listado-inbox');
+const btnNow = document.querySelector('#btn-now');
+const btnSoon = document.querySelector('#btn-soon');
+const btnProjects = document.querySelector('#btn-projects');
+const listadoNow = document.querySelector('#now');
+const listadoSoon = document.querySelector('#soon');
+const listadoProjects = document.querySelector('#projects');
+
+const objInbox = {
+    id = '',
+    texto = '',
+    now = '',
+    fecha = '',
+    proyecto = '',
+    done = '',
+};
+
+// Clases
+class Task {
+    constructor() {
+        this.task = [];
+    }
+
+    agregarTask(task) {
+
+    };
+
+    editarTask(taskActualizada) {
+
+    };
+
+    eliminarTask(id) {
+
+    };
+};
+
+class UI {
+
+}
+
+// Funciones
