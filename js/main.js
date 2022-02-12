d17aa2588b9b99c26833181e51558c0d69535bc8
Joaquin Morales <joaquin.moralestr@gmail.com>
2022-02-12 11:50:42 -0300

    Se implementa box de confuguracion para cada tarea

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ class UI {
 
         setTimeout(() => {
             divAlerta.remove();
-        }, 2500)
+        }, 2000)
     };
 
     mostrarInbox({tasks}) {
@@ -61,20 +61,74 @@ class UI {
 
         tasks.forEach(task => {
             const {id, texto} = task;
+            const newId = 'id' + id;
             
-            const divTask = document.createElement('div');
-            divTask.dataset.id = id;
-            divTask.classList.add('list-group-item');
+            const btnTask = document.createElement('button');
+            const divCollapsed = document.createElement('div');
+            const divCard = document.createElement('div');
+            const labelNow = document.createElement('label');
+            const inputNow = document.createElement('input');
+            const labelDate = document.createElement('label');
+            const inputDate = document.createElement('input');
+            const labelProject = document.createElement('label');
+            const inputProject = document.createElement('select');
+            const option = document.createElement('option');
 
-            divTask.innerHTML = 
+            btnTask.dataset.id = id;
+            btnTask.setAttribute('type', 'button');
+            btnTask.setAttribute('data-bs-toggle', 'collapse');
+            btnTask.setAttribute('data-bs-target', `#${newId}`);
+            btnTask.setAttribute('aria-expanded', 'false');
+            btnTask.setAttribute('aria-controls', newId);
+            btnTask.classList.add('list-group-item', 'list-group-item-action', 'list-group-item-light');
+
+            btnTask.innerHTML = 
             `
                 <input class="form-check-input me-1" type="checkbox" value="">
                 ${texto}
             `
 
-            listadoInbox.appendChild(divTask);
+            divCollapsed.setAttribute('id', newId);
+            divCollapsed.classList.add('collapse');
+            divCard.classList.add('card', 'card-body', 'd-flex', 'flex-row-lg', 'justify-content-between');
+
+            labelNow.classList.add('form-check-label', 'mb-3');
+            labelNow.setAttribute('for', 'input-now');
+            labelNow.textContent = 'Now?';
+            inputNow.classList.add('form-check-input', 'me-1');
+            inputNow.setAttribute('type', 'checkbox');
+            inputNow.setAttribute('id', 'input-now');
+            labelNow.appendChild(inputNow);
+
+            labelDate.classList.add('form-label', 'mb-3');
+            labelDate.setAttribute('for', 'input-date');
+            labelDate.textContent = 'Date';
+            inputDate.classList.add('form-control');
+            inputDate.setAttribute('type', 'date');
+            inputDate.setAttribute('id', 'input-date');
+            labelDate.appendChild(inputDate);
+
+            labelProject.classList.add('form-label', 'mb-3');
+            labelProject.setAttribute('for', 'input-project');
+            labelProject.textContent = 'Choose a project...';
+            inputProject.classList.add('form-select');
+            inputProject.setAttribute('aria-label', 'default select');
+            inputProject.setAttribute('id', 'input-project');
+            option.textContent = 'Empty';
+            inputProject.appendChild(option);
+            labelProject.appendChild(inputProject);
+
+            divCard.appendChild(labelNow);
+            divCard.appendChild(labelDate);
+            divCard.appendChild(labelProject);
+            divCollapsed.appendChild(divCard);
+
+            listadoInbox.appendChild(btnTask);
+            listadoInbox.appendChild(divCollapsed);
         })
     };
+
+    
 
     mostrarReview() {};
 
